f8654f1752799ae92896357ba4362b5395d50ec9
Koswara <koswara.c79@gmail.com>
2025-11-21 10:55:24 +0700

    feat: unit test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    include: ['__tests__/**/*.ts', '__tests__/**/*.tsx'],
+    exclude: ['__tests__/setupTests.ts'],
+    environment: 'jsdom',
+    setupFiles: ['__tests__/setupTests.ts']
+  }
 })
+// End of Vite config
